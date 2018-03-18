06161b57723d7ce61055baa80ebb393c27dfc91e
lochga01 <lochga01@luther.edu>
2018-03-18 14:53:55 -0500

    Local Storage - not functional yet

---
+++ pyServe.py
@@ -0,0 +1,18 @@
+from flask import Flask, Response
+import random, json
+import time
+
+app = Flask(__name__)
+
+@app.route('/getnum')
+def anyname():
+    time.sleep(2)
+    res = Response(json.dumps({'number':random.randrange(100)}))
+    #res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Content-type'] = 'application/json'
+    return res
+@app.route('/hello')
+def hello():
+    return "<h1>Hello World</h1>"
+
+app.run(debug = True, port = 5001)
