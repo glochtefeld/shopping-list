f76c7b3b6281a1e9243caf8ee1b6160ee8c77d6c
lochga01 <lochga01@luther.edu>
2018-03-21 23:43:11 -0500

    Removed detritus

--- routes.py
+++ routes.py
@@ -1,26 +1,20 @@
-<<<<<<< HEAD
-from flask import Flask, Response, render_template
-import random, json
-from listItem import ListItem
-
-=======
 from flask import Flask, Response, render_template, jsonify
->>>>>>> ghPages
+
 
 app = Flask(__name__)
 
 
-<<<<<<< HEAD
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
-=======
+
 
 @app.route('/') # This is to get the layouts working so that it redirects
 def index():
     return render_template("index.html")
->>>>>>> ghPages
+
 
 @app.route('/saveList')
 def saveList():
@@ -39,10 +33,6 @@ def getList():
 
     return jsonify(inPut)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ghPages
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5001)
