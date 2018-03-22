78b016174720684d8188aa972725a7f1ded6e15a
lochga01 <lochga01@luther.edu>
2018-03-21 20:07:06 -0500

    Routes is working again

--- routes.py
+++ routes.py
@@ -1,16 +1,12 @@
 from flask import Flask, Response, render_template
-import random, json
-import time
-
 
 app = Flask(__name__)
 
 
-'''
-@app.route('/') # This won't work on GHPages, so it's out on this branch.
+
+@app.route('/') # This is to get the layouts working so that it redirects
 def index():
-    return render_template("index2.html")
-'''
+    return render_template("index.html")
 
 @app.route('/saveList')
 #write json to file
@@ -18,24 +14,6 @@ def index():
 @app.route('/getList')
 # read json from file
 
-'''
-@app.route('/getnum')
-def anyname():
-    time.sleep(2)
-    res = Response(json.dumps({'number':random.randrange(100)}))
-    #res.headers['Access-Control-Allow-Origin'] = '*'
-    res.headers['Content-type'] = 'application/json'
-    return res
-@app.route('/hello')
-def hello():
-    return "<h1>Hello World</h1>"
 
-@app.route('/saveList')
-def saveToFile(file):
-
-    for row in table:
-        comma seperated identities
-        [name, quantity, price, ]
-    '''
 if __name__ == '__main__':
     app.run(debug = True, port = 5001)
